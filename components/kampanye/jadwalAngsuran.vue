e9e1371b86b82d53c2116aebf17d0b6f0760550f
sindystefani29 <46773589+sindystefani29@users.noreply.github.com>
2019-07-15 02:09:15 +0700

    jadwal angsuran

---
+++ components/kampanye/jadwalAngsuran.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="plr-15">
+        <div class="jadwal-angsuran" v-for="(item, index) in detailArr" :key="index">
+            <div class="jadwal-angsuran--container d-flex mb-3" v-for="(angsuran, index) in item.jadwal_angsuran" :key="index">
+                <div class="jadwal-angsuran--tanggal d-flex">
+                    <h4>12</h4>
+                    <span>JUL</span>
+                </div>
+                <div class="jadwal-angsuran--detail d-flex flex-1">
+                    <div>
+                        <span>{{angsuran.jenis}}</span>
+                        <p>{{angsuran.deskripsi}}</p>
+                    </div>
+                    <div>
+                        <h4>Rp {{angsuran.nominal}}</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { investasiList } from '../../assets/js/dummyData'
+export default {
+    props: {
+      param: String
+    },
+    data () {
+      return {
+        investasiList
+      }
+    },
+    computed: {
+      detailArr(){
+        let arr = this.investasiList.filter(field => this.param == field.id)
+        return arr
+      }
+    }
+}
+</script>
---
+++ pages/kampanye/_id/jadwal-angsuran.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main bg--white pt-65">
+    <topArrow 
+      :backLink = "backLink"
+      :transparent = "false"
+      textLink = "Estimasi Jadwal Pengembalian Angsuran"
+    />
+    <jadwalAngsuran 
+      :param = "paramId"
+    />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import jadwalAngsuran from '~/components/kampanye/jadwalAngsuran.vue'
+import topArrow from '~/components/layout/topArrow.vue'
+export default {
+  components: {
+    jadwalAngsuran,
+    topArrow
+  },
+  async asyncData({ params }) {
+    return {
+       paramId: params.id,
+       backLink: `/kampanye/${params.id}`
+    } 
+  }
+}
+</script>
