0f234226149f4fa44e80a0b8604a1d71e62d9c4f
Sindy Stefani <sindystefani7@gmail.com>
2019-08-19 13:08:50 +0700

    menu lainnya

--- pages/lainnya.vue
+++ pages/lainnya.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="main bg--white pb-50">
-    <portfolio/>
+    <menuLainnya/>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-import portfolio from '~/components/portfolio/portfolio.vue'
+import menuLainnya from '~/components/lainnya/menuLainnya.vue'
 export default {
   layout: 'bottomNavbar',
   components: {
-    portfolio
+    menuLainnya
   }
 }
 </script>
