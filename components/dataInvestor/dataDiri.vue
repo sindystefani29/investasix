a8c8a24c47a4945c632f21b7a03312538a996d9b
Sindy Stefani <sindystefani7@gmail.com>
2019-09-03 22:29:04 +0700

    data investor

---
+++ components/dataInvestor/dataDiri.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="data-diri plr-15">
+        <p class="text-grey mb-4">Mohon lengkapi data diri Anda di bawah</p>
+        <div class="d-flex flex-column">
+            <div class="data-diri--content d-flex justify-between mb-3">
+                <div class="d-flex align-center">
+                    <div class="data-diri--combo-btn mr-3 bg--green">
+                        <svg width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 3.72727L3.94737 7L9 1" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </div>
+                    <p><strong>KTP</strong></p>
+                </div>
+                <div class="d-flex align-center">
+                    <v-icon>chevron_right</v-icon>
+                </div>
+            </div>
+            <div class="data-diri--content d-flex justify-between mb-3">
+                <div class="d-flex align-center">
+                    <div class="data-diri--combo-btn mr-3 bg--grey"></div>
+                    <p><strong>Identitas Anda</strong></p>
+                </div>
+                <div class="d-flex align-center">
+                    <v-icon>chevron_right</v-icon>
+                </div>
+            </div>
+            <div class="data-diri--content d-flex justify-between mb-3">
+                <div class="d-flex align-center">
+                    <div class="data-diri--combo-btn mr-3 bg--grey"></div>
+                    <p><strong>Akun Bank</strong></p>
+                </div>
+                <div class="d-flex align-center">
+                    <v-icon>chevron_right</v-icon>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { investasiList } from '../../assets/js/dummyData'
+export default {
+    props: {
+      param: String
+    },
+    data () {
+      return {
+        investasiList
+      }
+    },
+    computed: {
+      detailArr(){
+        let arr = this.investasiList.filter(field => this.param == field.id)
+        return arr
+      }
+    }
+}
+</script>
---
+++ pages/data-investor/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main bg--white pt-65">
+    <topArrow 
+      backLink = "/lainnya"
+      :transparent = "false"
+      textLink = "Kelengkapan Data Investor"
+    />
+    <dataDiri/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import dataDiri from '~/components/dataInvestor/dataDiri.vue'
+import topArrow from '~/components/layout/topArrow.vue'
+export default {
+  scrollToTop: false,
+  components: {
+    dataDiri,
+    topArrow
+  }
+}
+</script>
